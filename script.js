a38772276cad58eb5385de5ba7b01e0f1dfb722b
Bharat Aggarwal <ritvikbharat5@gmail.com>
2026-02-09 13:16:01 +0000

    Added assets of the webpage, updated HTML & CSS, define the javascript

--- script.js
+++ script.js
@@ -1,2 +1,74 @@
-const screen = document.querySelector("screen");
-console.log("This is a screen");
+const greetingScreen = document.querySelector(".greeting-screen");
+const envelop = document.querySelector(".envelop");
+const frame = document.querySelector(".frame");
+const screen = document.querySelector(".screen");
+const minimize = document.querySelector(".minimize");
+const close = document.querySelector(".close");
+const question = document.querySelector(".question");
+const confirmMessage = document.querySelector(".confirm-message");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+envelop.addEventListener("click", function () {
+
+	greetingScreen.classList.add("hidden");
+	frame.classList.remove("hidden");
+
+	// Checking Height & width of Frame, screen, and noBtn
+
+	// Frame width & height
+	console.log("Frame Width & Height");
+	console.log(`Width: ${frame.offsetWidth}px`);
+	console.log(`Height: ${frame.offsetHeight}px`);
+
+	// Screen width & height
+	console.log("Screen Width & Height");
+	console.log(`Width: ${screen.offsetWidth}px`);
+	console.log(`Height: ${screen.offsetHeight}px`);
+
+	// noBtn width & height
+	console.log("noBtn Width & Height");
+	console.log(`Width: ${noBtn.offsetWidth}px`);
+	console.log(`Height: ${noBtn.offsetHeight}px`);
+
+});
+
+close.addEventListener("click", function () {
+	frame.classList.add("hidden");
+	greetingScreen.classList.remove("hidden");
+});
+
+minimize.addEventListener("click", function () {
+	frame.classList.add("hidden");
+	greetingScreen.classList.remove("hidden");
+});
+
+yesBtn.addEventListener("click", function () {
+	question.classList.add("hidden");
+	confirmMessage.classList.remove("hidden");
+});
+
+noBtn.addEventListener("pointerenter", () => {
+  const frameRect = frame.getBoundingClientRect();
+  const noBtnRect = noBtn.getBoundingClientRect();
+
+  console.log(frameRect);
+  console.log(noBtnRect);
+
+  const maxX = frameRect.width - noBtnRect.width;
+  const maxY = frameRect.height - noBtnRect.height;
+
+  console.log(maxX);
+  console.log(maxY);
+
+  const x = Math.random() * maxX;
+  const y = Math.random() * maxY;
+
+  noBtn.classList.add("run");
+
+  console.log(x);
+  console.log(y);
+
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
+});
